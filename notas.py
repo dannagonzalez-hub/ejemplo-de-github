efe6a76743874d65c04d2eddef7e73f9d2c4e598
Danna González <gonzalezdanna163@gmail.com>
2021-03-29 16:24:17 -0500

    subiendo codigo python

---
+++ notas.py
@@ -0,0 +1,19 @@
+codigo_estudiante = input("ingresa el número de código")
+print(codigo_estudiante)
+
+
+nota1 = float(input("introduce la nota más baja"))
+nota2 = float(input("introduce nota 2"))
+nota3 = float (input("introduce nota 3"))
+nota4 = float(input("introduce nota 4"))
+nota5 = float(input("introduce nota 5"))
+
+suma = nota1 + nota2 + nota3 + nota4 + nota5
+suma2= suma-nota1
+promedio = suma2 / 5   
+
+
+print('el promedio ajustado del estudiante es:' , promedio)
+
+
+
